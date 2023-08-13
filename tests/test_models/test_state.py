35c6f157788775b1aef76e4d8242dec71e17863d
rafmury <kariukimraphael@gmail.com>
2023-08-12 23:11:33 -0700

    AirBnB Clone

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,52 +1,48 @@
 #!/usr/bin/python3
-"""Unittest module for the State Class."""
-
+"""Module for test State class"""
 import unittest
-from datetime import datetime
-import time
-from models.state import State
-import re
 import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
+import pep8
+import datetime
+
+from models.state import State
 from models.base_model import BaseModel
 
 
 class TestState(unittest.TestCase):
+    """Test State class implementation"""
+    def test_doc_module(self):
+        """Module documentation"""
+        doc = State.__doc__
+        self.assertGreater(len(doc), 1)
 
-    """Test Cases for the State class."""
+    def test_pep8_conformance_state(self):
+        """Test that models/state.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    def test_pep8_conformance_test_state(self):
+        """Test that tests/test_models/test_state.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        res = pep8style.check_files(['tests/test_models/test_state.py'])
+        self.assertEqual(res.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_doc_constructor(self):
+        """Constructor documentation"""
+        doc = State.__init__.__doc__
+        self.assertGreater(len(doc), 1)
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_class(self):
+        """Validate the types of the attributes an class"""
+        with self.subTest(msg='Inheritance'):
+            self.assertTrue(issubclass(State, BaseModel))
 
-    def test_8_instantiation(self):
-        """Tests instantiation of State class."""
+        with self.subTest(msg='Attributes'):
+            self.assertIsInstance(State.name, str)
 
-        b = State()
-        self.assertEqual(str(type(b)), "<class 'models.state.State'>")
-        self.assertIsInstance(b, State)
-        self.assertTrue(issubclass(type(b), BaseModel))
-
-    def test_8_attributes(self):
-        """Tests the attributes of State class."""
-        attributes = storage.attributes()["State"]
-        o = State()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
+
